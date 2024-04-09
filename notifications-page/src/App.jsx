83da6175dd80b7779d149fa77ade7bea6bb9d909
= <=>
2024-04-09 23:55:54 +0300

    project initialization

---
+++ notifications-page/src/App.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+import NotificationsPage from "./pages/NotificationsPage"
+
+
+const App = () => {
+  return (
+    <div>
+        <NotificationsPage />
+    </div>
+  )
+}
+
+export default App
---
+++ notifications-page/src/pages/NotificationsPage.jsx
@@ -0,0 +1,14 @@
+
+import Card from '@mui/material/Card';
+
+const NotificationsPage = () => {
+  return (
+    <div>
+        <Card>
+            dftyjugkhnjklm
+        </Card>
+    </div>
+  )
+}
+
+export default NotificationsPage
